13e7421e5cff2a2fe654dbaf5925eca3450fba29
Kauan Santos <85942974+Kauan-afk@users.noreply.github.com>
2025-04-22 08:45:31 -0300

    Update Quadradinho.tsx

--- src/Components/Quadradinho.tsx
+++ src/Components/Quadradinho.tsx
@@ -4,7 +4,7 @@ import property from '../assets/dillon-kydd-pvdx8c6Y5BY-unsplash.jpg'
 export function Quadradinho (){
     return (
         <div className='max-w-60 bg-distaqueBlack rounded-2xl'>
-            <img src={property} className='w-full rounded-md ' alt="" />
+            <img src={property} className='w-full rounded-md ' alt="Nome do grupo" />
             <div className='p-3.5 pt-3.5'>
                 <h2 className='font-semibold text-3xl text-contrastWhite'>Lorem, ipsum.</h2>
             </div>
