885faf5aaa417963684cc5548f9837f5caf874d9
Camila Aparecida <114444962+CamilaApSoares@users.noreply.github.com>
2025-05-11 19:44:56 -0300

    Código atualizado
    código atualizado com a responsividade da página

--- src/pages/PageListGroup.tsx
+++ src/pages/PageListGroup.tsx
@@ -3,10 +3,10 @@ import { Quadradinho } from "@/Components/Quadradinho";
 import { FaPlusSquare } from "react-icons/fa";
 export function PageListGroup(){
     return(
-        <div className="flex flex-col w-full">
+        <div className="flex flex-col md:w-full">
             <Navbar/>
 
-        <div className='flex-1 border flex flex-col gap-5 border-contrastWhite30 border-b-0 border-r-0 rounded-tl-xl p-12 '>
+        <div className='flex-1 border flex flex-col md:gap-5 border-contrastWhite30 border-b-0 border-r-0 rounded-tl-xl md:p-12 '>
 
         <div className="text-contrastWhite flex justify-between">
             <h1 className="font-semibold text-4xl ">Grupos de imóveis</h1>
